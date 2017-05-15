6c899a9114dae295d326c241ac70c728c3ade54c
GregMotenJr <gmotenjr@gmail.com>
2017-05-15 11:12:03 -0400

    Initial commit

---
+++ WayneTechProjectMVC/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WayneTechProjectMVC.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        [Display(Name = "Category Name")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<Reviews> Reviews { get; set; }
+    }
+}
